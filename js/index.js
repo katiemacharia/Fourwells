63caea874a9acbaf6a9e43d9cd0a233bfa3b42fe
Harrisonkamau <kamauharrison87@gmail.com>
2017-06-10 22:04:35 +0300

    Append a paragraph after a set time

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
-window.onload = () => {
-  setTimeout(() => {
-    alert('hello');
-  }, 4000);
-}
+$(document).ready(function () {
+  // create a function that appends a paragraph to 
+  // the div with coming-soon id
+  function addText() {
+    $("#coming-soon").append(
+      "<p>Welcome to Fourwells!Currently, our site is under maintenance. We'll see you soon :)</p>"
+    )
+  }
+  // when the window(page) loads, delay 4s then add the paragraph
+  // the styling and animations of the added div can be seen on style.css
+  window.onload = () => {
+    setTimeout(() => {
+      addText();
+    }, 4000); // 4000 ms => 4 seconds!
+  }
+
+})
